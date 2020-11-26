d338bd7af83e5304325aa30bf1b88733291340af
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2020-11-27 02:42:03 +0900

    Add a feature that automatically get the permission as SuperUser.

--- app/src/main/java/jp/co/c_lis/ccl/morelocale/ui/help/PermissionRequiredDialog.kt
+++ app/src/main/java/jp/co/c_lis/ccl/morelocale/ui/help/PermissionRequiredDialog.kt
@@ -2,14 +2,24 @@ package jp.co.c_lis.ccl.morelocale.ui.help
 
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDialogFragment
+import com.stericson.RootShell.execution.Command
+import com.stericson.RootTools.RootTools
+import jp.co.c_lis.ccl.morelocale.BuildConfig
 import jp.co.c_lis.ccl.morelocale.R
+import timber.log.Timber
+import java.io.IOException
+import java.util.concurrent.TimeoutException
 
 class PermissionRequiredDialog : AppCompatDialogFragment() {
 
     companion object {
         val TAG = PermissionRequiredDialog::class.simpleName
+
+        private const val TIMEOUT = 15 * 1000
+        private const val COMMAND_PM_GRANT = "pm grant jp.co.c_lis.ccl.morelocale android.permission.CHANGE_CONFIGURATION"
 
         fun getInstance(): PermissionRequiredDialog {
             return PermissionRequiredDialog()
@@ -26,8 +36,30 @@ class PermissionRequiredDialog : AppCompatDialogFragment() {
                             .show(parentFragmentManager, HowToUsePmCommandDialog.TAG)
                 }
                 .setNeutralButton(R.string.execute_as_su) { _, _ ->
-
+                    if (getPermissionAsSuperUser()) {
+                        Toast.makeText(requireContext(),
+                                R.string.permission_granted, Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(requireContext(),
+                                R.string.could_not_get_su_privilege, Toast.LENGTH_LONG).show()
+                    }
                 }
                 .create()
+    }
+
+    private fun getPermissionAsSuperUser(): Boolean {
+        try {
+            RootTools.debugMode = BuildConfig.DEBUG
+            RootTools.getShell(true, TIMEOUT)
+                    .add(Command(0, COMMAND_PM_GRANT))
+            return true
+        } catch (e: InterruptedException) {
+            Timber.e(e)
+        } catch (e: IOException) {
+            Timber.e(e)
+        } catch (e: TimeoutException) {
+            Timber.e(e)
+        }
+        return false
     }
 }
