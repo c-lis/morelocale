9d4f524c02962f80f8cd759c8135027d1059b2a1
ARIYAMA Keiji <keiji_ariyama@c-lis.co.jp>
2020-12-06 01:42:33 +0900

    Refactoring

--- app/src/main/java/jp/co/c_lis/ccl/morelocale/repository/LocaleIsoRepository.kt
+++ app/src/main/java/jp/co/c_lis/ccl/morelocale/repository/LocaleIsoRepository.kt
@@ -1,7 +1,9 @@
 package jp.co.c_lis.ccl.morelocale.repository
 
 import android.app.Application
+import android.content.res.Resources
 import jp.co.c_lis.ccl.morelocale.MainApplication
+import jp.co.c_lis.ccl.morelocale.R
 import jp.co.c_lis.ccl.morelocale.entity.LocaleIsoItem
 import jp.co.c_lis.ccl.morelocale.entity.Type
 import kotlinx.coroutines.Dispatchers
@@ -13,8 +15,46 @@ abstract class LocaleIsoRepository(application: Application) {
 
     abstract val type: Type
 
-    suspend fun findAll() = withContext(Dispatchers.IO) {
+    suspend fun findAll(resources: Resources) = withContext(Dispatchers.IO) {
+        val list = db.localeIsoItemDao().findByType(type.name)
+        if (list.isNotEmpty()) {
+            return@withContext list
+        }
+
+        init(type, resources)
+
         return@withContext db.localeIsoItemDao().findByType(type.name)
+    }
+
+    private suspend fun init(type: Type, resources: Resources) = withContext(Dispatchers.Default) {
+        when (type) {
+            Type.Iso3166 -> {
+                init(resources.getStringArray(R.array.iso_3166_title),
+                        resources.getStringArray(R.array.iso_3166_value)
+                )
+            }
+            Type.Iso639 -> {
+                init(resources.getStringArray(R.array.iso_639_title),
+                        resources.getStringArray(R.array.iso_639_value))
+            }
+        }
+    }
+
+    private suspend fun init(
+            titles: Array<String>,
+            values: Array<String>
+    ) = withContext(Dispatchers.IO) {
+        titles.reverse()
+        values.reverse()
+
+        titles.forEachIndexed { index, title ->
+            val value = values[index]
+            add(LocaleIsoItem(
+                    label = title,
+                    value = value,
+                    isListed = true
+            ))
+        }
     }
 
     suspend fun findMatchLabel(text: String?) = withContext(Dispatchers.IO) {
--- app/src/main/java/jp/co/c_lis/ccl/morelocale/ui/locale_iso_list/LocaleIsoListViewModel.kt
+++ app/src/main/java/jp/co/c_lis/ccl/morelocale/ui/locale_iso_list/LocaleIsoListViewModel.kt
@@ -4,13 +4,9 @@ import android.content.res.Resources
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import jp.co.c_lis.ccl.morelocale.R
 import jp.co.c_lis.ccl.morelocale.entity.LocaleIsoItem
-import jp.co.c_lis.ccl.morelocale.entity.Type
 import jp.co.c_lis.ccl.morelocale.repository.LocaleIsoRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class LocaleIsoListViewModel(
         private val localeIsoRepository: LocaleIsoRepository
@@ -20,45 +16,7 @@ class LocaleIsoListViewModel(
 
     fun init(resources: Resources) {
         viewModelScope.launch {
-            val list = localeIsoRepository.findAll()
-            if (list.isNotEmpty()) {
-                localeIsoList.postValue(list)
-                return@launch
-            }
-
-            when (localeIsoRepository.type) {
-                Type.Iso3166 -> {
-                    init(
-                            resources.getStringArray(R.array.iso_3166_title),
-                            resources.getStringArray(R.array.iso_3166_value)
-                    )
-                }
-                Type.Iso639 -> {
-                    init(
-                            resources.getStringArray(R.array.iso_639_title),
-                            resources.getStringArray(R.array.iso_639_value)
-                    )
-                }
-            }
-
-            localeIsoList.postValue(localeIsoRepository.findAll())
-        }
-    }
-
-    private suspend fun init(
-            titles: Array<String>,
-            values: Array<String>
-    ) = withContext(Dispatchers.Default) {
-        titles.reverse()
-        values.reverse()
-
-        titles.forEachIndexed { index, title ->
-            val value = values[index]
-            localeIsoRepository.add(LocaleIsoItem(
-                    label = title,
-                    value = value,
-                    isListed = true
-            ))
+            localeIsoList.postValue(localeIsoRepository.findAll(resources))
         }
     }
 
